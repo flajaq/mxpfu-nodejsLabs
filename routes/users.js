e64a43a5e458067c6e0e6db8ae167faba863640b
flajaq <flajaq12@gmail.com>
2023-01-04 08:06:42 -0500

    finished CRUD lab

--- routes/users.js
+++ routes/users.js
@@ -25,8 +25,7 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send(users);
+    res.send(JSON.stringify({users},null,4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
@@ -34,6 +33,27 @@ router.get("/:email",(req,res)=>{
     const email = req.params.email;
     let filtered_users = users.filter((user) => user.email == email);
     res.send(filtered_users);
+});
+
+router.get("/lastName/:lastName",(req,res)=>{
+    const lastName = req.params.lastName;
+    let filtered_users = users.filter((user) => user.lastName == lastName);
+    res.send(filtered_users);
+});
+
+function getDateFromString(strDate) {
+    let [dd,mm,yyyy] = strDate.split('-')
+    return new Date(yyyy+"/"+mm+"/"+dd);
+}
+    
+// console.log(sorted_users);
+router.get("/sort",(req,res)=>{
+    let sorted_users=users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+            return d1-d2;
+          });
+    res.send(sorted_users);
 });
 
 
@@ -50,7 +70,8 @@ router.put("/:email", (req, res) => {
     if (filtered_users.length > 0) {
         let filtered_user = filtered_users[0];
         let DOB = req.query.DOB;
-        //if the DOB has changed
+        let firstName = req.query.firstName;
+        let lastName = req.query.lastName;
         if(DOB) {
             filtered_user.DOB = DOB
         }
@@ -72,8 +93,9 @@ router.put("/:email", (req, res) => {
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    const email = req.params.email;
+    users = users.filter((user) => user.email != email);
+    res.send(`User with the email  ${email} deleted.`);
 });
 
 module.exports=router;
